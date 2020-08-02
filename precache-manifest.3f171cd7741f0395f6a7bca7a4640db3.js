3be329f5957930d55c199624b6ca33533f0877a5
vacic <nikolavacic@ymail.com>
2020-08-02 15:30:59 +0200

    Updates

---
+++ precache-manifest.3f171cd7741f0395f6a7bca7a4640db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de396cb26f8539f9d1d8859e9f667f5b",
+    "url": "/game_thing/index.html"
+  },
+  {
+    "revision": "1e4830f0b939e313128e",
+    "url": "/game_thing/static/css/main.ac783ccf.chunk.css"
+  },
+  {
+    "revision": "d0ed38a3a46a7737f12e",
+    "url": "/game_thing/static/js/2.49d05a2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game_thing/static/js/2.49d05a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e4830f0b939e313128e",
+    "url": "/game_thing/static/js/main.8d1b445d.chunk.js"
+  },
+  {
+    "revision": "ab0c894ac7ab093d430d",
+    "url": "/game_thing/static/js/runtime-main.cd607a02.js"
+  },
+  {
+    "revision": "a87f14b91d7b1081ee6eb79e71bba090",
+    "url": "/game_thing/static/media/loading.a87f14b9.png"
+  }
+]);
