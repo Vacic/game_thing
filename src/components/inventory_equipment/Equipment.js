99294164d0d9a67a2e05bf85616a22d88a3bf0d5
vacic <nikolavacic@ymail.com>
2020-08-02 14:46:51 +0200

    -

--- src/components/inventory_equipment/Equipment.js
+++ src/components/inventory_equipment/Equipment.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
 
 export default class Equipment extends Component {
-    hello = () => {
-        const str = 'Helmet'
-        console.log(str.match("Wooden Helmet"))
-    }
     render() {
         return (
             <div className="equipment">
                 <h3>Equipment</h3>
                 
-                <div className="equipment-slots" onClick={() => this.hello()}>
+                <div className="equipment-slots">
                     <div className="helmet"></div>
                     <div className="weapon"></div>
                     <div className="armor"></div>
                     <div className="shield"></div>
                     <div className="pants"></div>
                     <div className="boots"></div>
-
-                    {/* <img className="helmet" alt=""></img>
-                    <img className="weapon" alt=""></img>
-                    <img className="armor" alt=""></img>
-                    <img className="shield" alt=""></img>
-                    <img className="pants" alt=""></img>
-                    <img className="boots" alt=""></img> */}
                 </div>
             </div>
         )
