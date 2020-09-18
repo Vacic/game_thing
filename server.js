bcfd8eb932e81654c9dffa002c3b97a2dec77bff
Vacic <nikolavacic@ymail.com>
2020-09-18 12:47:30 +0200

    changes for production build

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 connectDB();
-
+// Middleware
 app.use(express.json());
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-
+// Routes
 app.use('/users', require('./routes/users'));
 app.use('/auth', require('./routes/auth'));
+
+// Serve static assets in production
+if(PerformanceObserverEntryList.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
 
