56850d82a7b9227b9ca4fb3fae34c5d4e70c875a
Vacic <nikolavacic@ymail.com>
2020-09-22 15:39:20 +0200

    test

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -2,14 +2,11 @@ const jwt = require('jsonwebtoken');
 
 const checkToken = (req, res, next) => {
     let cookieToken = req.cookies.token;
-    console.log(cookieToken)
-
-
     if(!cookieToken || typeof cookieToken === 'undefined') return res.status(403).json({error: 'Token Not Provided'})
 
-    cookieToken = bearerToken.split(' ')
-    const bearer = bearerToken[0];
-    const token = bearerToken[1];
+    cookieToken = cookieToken.split(' ')
+    const bearer = cookieToken[0];
+    const token = cookieToken[1];
 
     if(bearer !== 'Bearer') return res.status(403).json({ error: "Unauthorized Request" });
     if(!token) return res.status(403).json({ error: "Unauthorized Request" });
